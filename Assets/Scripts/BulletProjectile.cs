a72e74e5c660e1afec7d8832849e3c9aeaf82296
CodeUser11047 <zhangzizi110@gmail.com>
2023-09-20 00:00:35 +0800

    子弹效果

--- Assets/Scripts/BulletProjectile.cs
+++ Assets/Scripts/BulletProjectile.cs
@@ -7,12 +7,13 @@ public class BulletProjectile : MonoBehaviour
     [SerializeField] private float moveSpeed = 200f;
     [SerializeField] private Vector3 targetOffest = new(0, 1f, 0);
     [SerializeField] private TrailRenderer trailRenderer;
+    [SerializeField] private Transform bulletHitVFXPrefab;
     private Vector3 targetPosition;
 
     private void Update()
     {
         //哥哥我只飞一帧！
-        Vector3 moveDir = ((targetPosition + targetOffest) - transform.position).normalized;
+        Vector3 moveDir = (targetPosition + targetOffest - transform.position).normalized;
         float distanceBeforeMoving = Vector3.Distance(targetPosition, transform.position);
 
         transform.position += moveSpeed * Time.deltaTime * moveDir;
@@ -21,7 +22,12 @@ public class BulletProjectile : MonoBehaviour
 
         if (distanceBeforeMoving < distanceAfterMoving)
         {
+            //并不直接通过碰撞来检测，因为本游戏采用的是回合制，可以直接在对应的敌人脚本上扣除生命
+            //因为右trail所以在最后的时刻把position设置在对应位置上以便于trail显示正确
+            transform.position = targetPosition + targetOffest;
+            Instantiate(bulletHitVFXPrefab, targetPosition + targetOffest, Quaternion.identity);
             trailRenderer.transform.parent = null;
+
             Destroy(gameObject);
         }
     }
